58b7dc05a998b707f803040d116874fc10b6c0c6
Tommy Truong <ttthao@ucsd.edu>
2018-02-28 00:46:29 -0800

    Add Props tutorial work

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Text, StyleSheet, View, Image } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
+      <Text>Hello sir {this.props.name}!</Text>
+    )
+  }
+}
+
+export default class App extends Component {
+  render() {
+    let pic = {
+      uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
+    return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Hello Tommy!!!</Text>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+        <Greeting name="Thomas" />
+        <Greeting name="Phil" />
       </View>
     );
   }
